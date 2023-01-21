f123bfa8f3afd5ca586a087dc13741325235680e
EunJu JO <101851472+dmswn1004@users.noreply.github.com>
2023-01-21 19:54:03 +0900

    Update 평행.java

--- Level_0/평행.java
+++ Level_0/평행.java
@@ -1,19 +1,18 @@
 class Solution {
-  //기울기 구하는 함수
+    //기울기 구하는 함수
     public double inclination(int dot1[], int dot2[]){
         return (double)(dot2[1]-dot1[1])/(dot2[0]-dot1[0]);
     }
     
     public int solution(int[][] dots) {
-//         if(inclination(dots[0],dots[1]) == inclination(dots[2],dots[3]) || inclination(dots[0],dots[2])==inclination(dots[1],dots[3])||inclination(dots[0],dots[3])==inclination(dots[1],dots[2]))
-//             return 1;
-           if(inclination(dots[0],dots[1]) == inclination(dots[2],dots[3]))
+//      if(inclination(dots[0],dots[1]) == inclination(dots[2],dots[3]) || inclination(dots[0],dots[2])==inclination(dots[1],dots[3])||inclination(dots[0],dots[3])==inclination(dots[1],dots[2]))
+//          return 1;
+        if(inclination(dots[0],dots[1]) == inclination(dots[2],dots[3]))
             return 1;
-           if(inclination(dots[0],dots[2]) == inclination(dots[1],dots[3]))
+        if(inclination(dots[0],dots[2]) == inclination(dots[1],dots[3]))
             return 1;
-           if(inclination(dots[0],dots[3]) == inclination(dots[1],dots[2]))
+        if(inclination(dots[0],dots[3]) == inclination(dots[1],dots[2]))
             return 1;
-        
-           return 0;
+        return 0;
     }
 }
