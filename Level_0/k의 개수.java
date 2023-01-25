036d2d6ea03170aa65bf3dd8fa4c5b4bfbf9fcb6
EunJu JO <101851472+dmswn1004@users.noreply.github.com>
2023-01-26 01:20:49 +0900

    Create k의 개수.java

---
+++ Level_0/k의 개수.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int i, int j, int k) {
+        int answer = 0;
+        for(int l = i; l <= j; l++){
+            StringBuilder sb = new StringBuilder();
+            sb.append(l);
+            for(int r = 0; r < sb.length(); r++){
+                if(((sb.charAt(r))-'0')==k){
+                    answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
