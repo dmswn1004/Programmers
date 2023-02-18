23ddd8b62f2dd9fe3973b6f630975d5ec6a17fc4
EunJu JO <dmswnehf@naver.com>
2023-02-18 19:44:28 +0900

    Update 약수의 개수와 덧셈.py

--- Level_1/약수의 개수와 덧셈.py
+++ Level_1/약수의 개수와 덧셈.py
@@ -11,3 +11,14 @@ def solution(left, right):
             
     return answer
   
+# 개선된 풀이 (시간 ↓)
+import math
+
+def solution(left, right):
+    answer = 0
+    for i in range(left, right + 1, 1):
+        if int(math.sqrt(i)) == math.sqrt(i):
+            answer -= i
+        else:
+            answer += i
+    return answer
