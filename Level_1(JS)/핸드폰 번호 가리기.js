5f61e834d8d2215a5ebde4b430f60750771eee93
EunJu JO <dmswnehf@naver.com>
2023-03-09 19:18:02 +0900

    Create 핸드폰 번호 가리기.js

---
+++ Level_1(JS)/핸드폰 번호 가리기.js
@@ -0,0 +1,9 @@
+function solution(phone_number) {
+    var answer = phone_number.slice(phone_number.length-4, phone_number);
+    return  "*".repeat(phone_number.length-4,) + answer;
+}
+
+function solution(phone_number) {
+    var answer = phone_number.slice(-4);
+    return  "*".repeat(phone_number.length-4,) + answer;
+}
