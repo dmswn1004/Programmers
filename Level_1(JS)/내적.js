0dfa4da231abfe4af5ed677cc1a358fe9796aaaa
EunJu JO <dmswnehf@naver.com>
2023-03-12 03:06:55 +0900

    Create 내적.js

---
+++ Level_1(JS)/내적.js
@@ -0,0 +1,11 @@
+function solution(a, b) {
+    var answer = 0;
+    for (var i = 0; i < a.length; i++){
+        answer += a[i] * b[i];
+    }
+    return answer;
+}
+
+function solution(a, b) {
+    return a.reduce((acc, cur, i)=>(acc += cur*b[i]), 0);
+}
