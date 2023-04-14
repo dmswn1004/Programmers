33a01d1c26e727ac2f5eb1eec15a4380dda68edc
EunJu JO <dmswnehf@naver.com>
2023-04-15 01:01:55 +0900

    Update 추억 점수.js

--- Level_1(JS)/추억 점수.js
+++ Level_1(JS)/추억 점수.js
@@ -1,4 +1,17 @@
 function solution(name, yearning, photo) {
+    var answer = [];
+    for(var i = 0; i < photo.length; i++){
+        var sum = 0;
+        for(var j = 0; j < photo[i].length; j++){
+            if(name.indexOf(photo[i][j]) != -1) sum += yearning[name.indexOf(photo[i][j])];
+        }
+        answer.push(sum);
+    }
+    return answer;
+}
+
+// 다른 사람의 풀이
+function solution(name, yearning, photo) {
     let obj = {};
     for(let i = 0; i < name.length; i++){
         obj[name[i]] = yearning[i];
