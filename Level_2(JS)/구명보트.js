7b713da79e84ecb6cd1689477928afc6b4bb7568
EunJu JO <dmswnehf@naver.com>
2023-05-14 17:40:39 +0900

    Update 구명보트.js

--- Level_2(JS)/구명보트.js
+++ Level_2(JS)/구명보트.js
@@ -1,10 +1,9 @@
 function solution(people, limit) {
-    var answer = people.length;
-    var sum = people[0];
-    people.sort((a,b)=>(a-b));
-    for(var i = 0; i < people.length; i++){
-        if(people[i] + people[i+1] <= limit) answer--;
-            if(people[i] + people[i+1] + people[i+2] <= limit) answer--;
-    }      
+    var answer =0;
+    people.sort ((a,b) => b-a);
+    for (var i=0, j = people.length - 1; i <=j ; i++) {
+        if (people[i] + people [j] <= limit) j--
+        answer ++
+    }
     return answer;
 }
