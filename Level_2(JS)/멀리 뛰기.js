69f39312dbac2535e00877cef9e8e19579cb3a9a
EunJu JO <dmswnehf@naver.com>
2023-05-30 01:58:17 +0900

    Update 멀리 뛰기.js

--- Level_2(JS)/멀리 뛰기.js
+++ Level_2(JS)/멀리 뛰기.js
@@ -1,8 +1,9 @@
 function solution(n) {
-    const dp = Array.from({length:n}).fill(0);
-    dp[0] = 1, dp[1] = 1;
-  	for (let i=2; i<=n; i++) {
-        dp[i] = (dp[i-2] + dp[i-1]) % 1234567;
-    }
-    return dp[n];
+  const dp = Array(n + 1).fill(0);
+  dp[1] = 1;
+  dp[2] = 2;
+  for (let i = 3; i <= n; i++) {
+    dp[i] = (dp[i - 1] + dp[i - 2]) % 1234567;
+  }
+  return dp[n]; 
 }
